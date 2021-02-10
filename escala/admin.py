214a80eb88ee7cbcc6778cf20b1aefa21d0e0734
Tiago Chaves <tiagodevch@gmail.com>
2021-02-10 02:01:57 -0300

    Trabalhando no ministerio

--- escala/admin.py
+++ escala/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import *
 
 
+
+@admin.register(Ministerio)
+class MinisterioAdmin(admin.ModelAdmin):
+    list_display = ['nome_ministerio']
+
+
 @admin.register(Pessoa)
 class PessoaAdmin(admin.ModelAdmin):
     list_display = ['nome', 'email', 'celular']
@@ -9,9 +15,9 @@ class PessoaAdmin(admin.ModelAdmin):
 
 @admin.register(Escala)
 class EscalaAdmin(admin.ModelAdmin):
-    list_display = ['nome', 'dia', 'get_data', 'get_ultima_alteracao']
-
-
+    list_display = ['nome', 'get_data', 'get_ministerio', 'get_ultima_alteracao']
+    list_filter = ['ministerio', 'data']
+    search_fields = ['nome']
 
 
 
--- escala/models.py
+++ escala/models.py
@@ -1,30 +1,40 @@
 from django.db import models
 
 
+class Ministerio(models.Model):
+    nome_ministerio = models.CharField(max_length=30)
+    funcao = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nome_ministerio
+
+
 class Pessoa(models.Model):
     nome = models.CharField(max_length=25)
+    sobre_nome = models.CharField(max_length=25)
+    # ministerio = models.ForeignKey('Ministerio', on_delete=models.DO_NOTHING, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     celular = models.CharField(max_length=11)
 
     def __str__(self):
-        return self.nome
+        return f'{self.nome} {self.sobre_nome}'
 
 
 class Escala(models.Model):
-    choice = [
-        ['man', 'Manhã'],
-        ['noi', 'Noite'],
-    ]
     nome = models.ForeignKey('Pessoa', on_delete=models.CASCADE)
-    dia = models.CharField(max_length=3, choices=choice, default='noi')
-    data = models.DateField()
+    data = models.DateTimeField()
+    ministerio = models.ForeignKey('Ministerio', on_delete=models.DO_NOTHING)
     ultima_alteração = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.nome)
+        ministerio = str(self.ministerio)
+        return f'{str(self.nome)} {ministerio}'
+
+    def get_ministerio(self):
+        return f' {self.ministerio}'
 
     def get_data(self):
-        return self.data.strftime('%d/%m/%Y')
+        return self.data.strftime('%d/%m/%Y %H:%M')
 
     def get_ultima_alteracao(self):
         return self.ultima_alteração.strftime('%d/%m/%Y %H:%M')
