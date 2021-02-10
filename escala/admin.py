bac021aea24cc457f8b2cc00af6ce560b6df40b7
Tiago <Tiagolch@hotmail.com>
2021-02-10 13:53:44 -0300

    separado ministerio com funcao

--- escala/admin.py
+++ escala/admin.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from .models import *
 
 
-
 @admin.register(Ministerio)
 class MinisterioAdmin(admin.ModelAdmin):
-    list_display = ['nome_ministerio']
+    list_display = ['nome_ministerio', 'funcao']
 
 
 @admin.register(Pessoa)
@@ -15,8 +14,8 @@ class PessoaAdmin(admin.ModelAdmin):
 
 @admin.register(Escala)
 class EscalaAdmin(admin.ModelAdmin):
-    list_display = ['nome', 'get_data', 'get_ministerio', 'get_ultima_alteracao']
-    list_filter = ['ministerio', 'data']
+    list_display = ['nome', 'get_data' ,'get_ultima_alteracao']
+    list_filter = [ 'data']
     search_fields = ['nome']
 
 
--- escala/models.py
+++ escala/models.py
@@ -3,16 +3,24 @@ from django.db import models
 
 class Ministerio(models.Model):
     nome_ministerio = models.CharField(max_length=30)
-    funcao = models.CharField(max_length=30)
 
     def __str__(self):
         return self.nome_ministerio
 
 
+class Funcao(models.Model):
+    ministerio = models.ForeignKey(Ministerio, on_delete=models.CASCADE)
+    funcao = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{str(self.ministerio)} {self.funcao}'
+
+
 class Pessoa(models.Model):
     nome = models.CharField(max_length=25)
     sobre_nome = models.CharField(max_length=25)
-    # ministerio = models.ForeignKey('Ministerio', on_delete=models.DO_NOTHING, blank=True, null=True)
+    ministerio = models.ForeignKey('Ministerio', on_delete=models.DO_NOTHING, blank=True, null=True)
+    funcao = models.ForeignKey(Funcao, on_delete=models.CASCADE)
     email = models.EmailField(blank=True, null=True)
     celular = models.CharField(max_length=11)
 
@@ -21,14 +29,13 @@ class Pessoa(models.Model):
 
 
 class Escala(models.Model):
+    funcao = models.ForeignKey(Funcao, on_delete=models.CASCADE)
     nome = models.ForeignKey('Pessoa', on_delete=models.CASCADE)
     data = models.DateTimeField()
-    ministerio = models.ForeignKey('Ministerio', on_delete=models.DO_NOTHING)
     ultima_alteração = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        ministerio = str(self.ministerio)
-        return f'{str(self.nome)} {ministerio}'
+        return f'{str(self.nome)}'
 
     def get_ministerio(self):
         return f' {self.ministerio}'
