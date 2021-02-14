a37c4133bc7b3e48029f84cd3e0d7778c066c846
Tiago Chaves <tiagodevch@gmail.com>
2021-02-14 09:54:32 -0300

    Acertado e configurado static

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,7 @@
 
 
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,5 +109,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+    '/static/img/'
+]
 
